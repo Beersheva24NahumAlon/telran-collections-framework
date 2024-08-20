5db5bd3f3117648146e3242a510ad14ebc570110
Nahum Alon <nochum.shepsl@gmail.com>
2024-08-20 17:42:40 +0300

    Project created

---
+++ src/main/java/telran/util/Collection.java
@@ -0,0 +1,24 @@
+package telran.util;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public interface Collection<T> extends Iterable<T> {
+    boolean add(T obj);
+
+    boolean remove(T pattern);
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean contains(T pattern);
+
+    default Stream<T> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
+
+    default Stream<T> parallel() {
+        return StreamSupport.stream(spliterator(), true);
+    }
+}
---
+++ src/test/java/telran/util/ListTest.java
@@ -0,0 +1,11 @@
+package telran.util;
+
+public abstract class ListTest extends CollectionTest {
+    List<Integer> list;
+
+    @Override
+    void setUp() {
+        super.setUp();
+        list = (List<Integer>) collection;
+    }
+}
