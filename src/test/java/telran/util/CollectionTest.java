4fdcc611d08021cd0b119d3670e43148fbdb450c
Nahum Alon <nochum.shepsl@gmail.com>
2024-08-21 19:17:28 +0300

    Realized all methods in class ArrayList and all tests for them

--- src/test/java/telran/util/CollectionTest.java
+++ src/test/java/telran/util/CollectionTest.java
@@ -1,10 +1,12 @@
 package telran.util;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,9 +20,10 @@ public abstract class CollectionTest {
 
     @Test
     void addTest() {
+        Integer[] expected = { 3, -10, 20, 1, 10, 8, 100, 17, 200, 17 };
         assertTrue(collection.add(200));
         assertTrue(collection.add(17));
-        assertEquals(array.length + 2, collection.size());
+        assertArrayEquals(expected, collection.stream().toArray());
     }
 
     @Test
@@ -30,20 +33,42 @@ public abstract class CollectionTest {
 
     @Test
     void removeTest() {
+        Integer[] expected = { -10, 20, 1, 10, 8, 100 };
         assertFalse(collection.remove(4));
         assertTrue(collection.remove(3));
         assertTrue(collection.remove(17));
-        assertEquals(array.length - 2, collection.size());
+        assertArrayEquals(expected, collection.stream().toArray());
     }
 
     @Test
     void isEmptyTest() {
         assertFalse(collection.isEmpty());
+        assertTrue(collection.remove(3));
+        assertTrue(collection.remove(-10));
+        assertTrue(collection.remove(20));
+        assertTrue(collection.remove(1));
+        assertTrue(collection.remove(10));
+        assertTrue(collection.remove(8));
+        assertTrue(collection.remove(100));
+        assertTrue(collection.remove(17));
+        assertTrue(collection.isEmpty());
     }
 
     @Test
     void containsTest() {
         assertTrue(collection.contains(-10));
         assertFalse(collection.contains(-11));
+    }
+
+    @Test
+    void iteratorTest() {
+        Iterator<Integer> iterator = collection.iterator();
+        Integer[] expected = { 3, -10, 20, 1, 10, 8, 100, 17 };
+        Integer[] actual = new Integer[expected.length];
+        int i = 0;
+        while (iterator.hasNext()) {
+            actual[i++] = iterator.next();
+        }
+        assertArrayEquals(expected, actual);
     }
 }
--- src/test/java/telran/util/ListTest.java
+++ src/test/java/telran/util/ListTest.java
@@ -1,5 +1,11 @@
 package telran.util;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import org.junit.jupiter.api.Test;
+
 public abstract class ListTest extends CollectionTest {
     List<Integer> list;
 
@@ -9,5 +15,41 @@ public abstract class ListTest extends CollectionTest {
         list = (List<Integer>) collection;
     }
 
-    
+    @Test
+    @Override
+    void addTest() {
+        Integer[] expected = { 3, -10, 20, 1, 1, 10, 8, 100, 17, 17 };
+        list.add(3, 1);
+        list.add(8, 17);
+        assertArrayEquals(expected, list.stream().toArray());
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> list.add(13, 20));
+    }
+
+    @Test
+    @Override
+    void removeTest() {
+        Integer[] expected = { -10, 20, 1, 10, 8, 100 };
+        list.remove(0);
+        list.remove(6);
+        assertArrayEquals(expected, list.stream().toArray());
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> list.remove(20));
+    }
+
+    @Test
+    void indexOfTest() {
+        list.add(3, 20);
+        assertEquals(2, list.indexOf(20));
+    }
+
+    @Test
+    void getTest() {
+        assertEquals(100, list.get(6));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> list.get(20));
+    }
+
+    @Test
+    void lastIndexOfTest() {
+        list.add(3, 20);
+        assertEquals(3, list.lastIndexOf(20));
+    }
 }
