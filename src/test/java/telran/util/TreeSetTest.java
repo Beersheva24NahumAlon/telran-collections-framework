c64f390600d85deef696fc7e12f9d5427af4b70b
Nahum Alon <nochum.shepsl@gmail.com>
2024-09-17 22:15:51 +0300

    Fixed some warnings

--- src/test/java/telran/util/TreeSetTest.java
+++ src/test/java/telran/util/TreeSetTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.stream.IntStream;
 
